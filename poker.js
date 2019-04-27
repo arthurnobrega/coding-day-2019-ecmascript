a5a65da46daf7af3969cc45e0b3492de50d55c55
Arthur Nobrega <arthurthiago@gmail.com>
2019-04-27 16:49:42 -0300

    Cartas validadas e numero de cartas na mao

--- poker.js
+++ poker.js
@@ -1,3 +1,17 @@
 export function checkFiveCards(hand) {
   return hand.split(' ').length === 5;
 }
+
+export function checkCard(card) {
+  return card.length === 2 && validateSuit(card) && validateValue(card);
+}
+
+function validateSuit(card) {
+  const suits = ['S', 'H', 'D', 'C'];
+  return !!suits.filter(s => s === card[1]).length;
+}
+function validateValue(card) {
+  const values = ['2', '3', '4', '5', '6', '7', '8', '9',
+    'T', 'J', 'Q', 'K', 'A'];
+  return !!values.filter(s => s === card[0]).length;
+}
--- poker.test.js
+++ poker.test.js
@@ -1,4 +1,4 @@
-import { checkFiveCards } from './poker'
+import { checkFiveCards, checkCard } from './poker';
 
 describe('Checar 5 cartas', () => {
   test('checa mão com cinco cartas', () => {
@@ -6,5 +6,26 @@ describe('Checar 5 cartas', () => {
   });
   test('checa mão com 4 cartas', () => {
     expect(checkFiveCards('2H 3D 5S 9C')).toEqual(false);
+  });
+});
+
+describe('Checar carta válida', () => {
+  test('checa se uma carta válida passada', () => {
+    expect(checkCard('2H')).toEqual(true);
+  });
+  test('checa se uma carta de tamanho inválido é passada', () => {
+    expect(checkCard('2')).toEqual(false);
+  });
+  test('checa se uma carta de naipe inválido é passada', () => {
+    expect(checkCard('22')).toEqual(false);
+  });
+  test('checa se uma carta de naipe valido é passada', () => {
+    expect(checkCard('2C')).toEqual(true);
+  });
+  test('checa se uma carta de valor inválido é passada', () => {
+    expect(checkCard('0C')).toEqual(false);
+  });
+  test('checa se uma carta de valor valido é passada', () => {
+    expect(checkCard('2C')).toEqual(true);
   });
 });
